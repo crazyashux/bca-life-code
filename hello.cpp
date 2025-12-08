92730c9b50703f3670110bbdc09ddca5dc94ed38
ASHU.X <187722143+crazyashux@users.noreply.github.com>
2025-12-08 18:04:21 +0530

    area program
    a glata program hai !

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>  //kya a program sahi hai ?  mera ansur sir galat batya hai !
+void main()
+{
+float leanth,brath,area,perimeter;
+
+    cout<<"Enter a leanth";
+    cin>>leanth;
+
+    cout<<"Enter a breath";
+    cin>>brath;
+
+    area=leanth*brath;
+    perimeter=2*(leanth+brath);
+    cout<<"area"<<area;
+    cout<<"perimeter"<<perimeter;
+}
+  
